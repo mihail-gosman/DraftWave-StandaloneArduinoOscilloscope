d460b2fb6c70e809a6b4f863e46118a1115f654b
mihail-gosman <mihailc.gosman@gmail.com>
2024-08-17 21:33:39 +0200

    update

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,11 +1,32 @@
 void setup() {
-  Serial.begin(9600);
-  pinMode(A0, INPUT);
+  DDRB = 0xFF;
+  DDRD = 0x00;
+  PORTD = 0xFF;
 }
 
 void loop() {
-  long a = millis();
-  //Serial.println(analogRead(A0));
-  a = millis() - a;
-  Serial.println(a);
+  PORTB ^= 0xFF;
+
+  while ((PIND & 0xFF) != 0b11111011) {
+  }
+
+  PORTB = 0b10101010;
+
+  while ((PIND & 0xFF) != 0b11110111) {
+  }
+
+  for (uint8_t i = 0; i < 8; i++) {
+    PORTB = (1 << i);
+    delay(100);
+  }
+
+  PORTB ^= 0xFF;
+  delay(1000);
+
+  for (uint8_t i = 0; i < 8; i++) {
+    PORTB = (PORTB << 1) | (PORTB >> 7);
+    delay(100);
+  }
+
+  PORTB = 0x00;
 }
