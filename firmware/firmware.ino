e69cc78a485d736964ebe3a31aae97ca454263a5
mihail-gosman <mihailc.gosman@gmail.com>
2024-09-01 17:43:10 +0200

    Update firmware.ino

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,32 +1,40 @@
+unsigned long delta = millis();
+int analogPins[] = { A0, A1, A2, A3, A4, A5 };
+int analogInput[6];
+
+void startMeasurement() {
+  TCCR1B |= (1 << CS10);
+  TIMSK1 |= (1 << TOIE1);
+}
+
+void stopMeasurement() {
+  TCCR1B |= (1 << CS10);
+  TIMSK1 |= (1 << TOIE1);
+}
+
 void setup() {
-  DDRB = 0xFF;
-  DDRD = 0x00;
-  PORTD = 0xFF;
+  Serial.begin(115200);
+  
+  TCCR1A = 0;
+  TCCR1B = 0;
+  
+  for (int i=0; i<6; i++) {
+    pinMode(analogPins[i], INPUT);
+  }  
+}
+
+ISR(TIMER1_OVF_vect) {
+  for (int i=0; i<6; i++) {
+    analogInput[i] = analogRead(analogPins[i]);
+  }
+
+  for (int i=0; i<6; i++) {
+    Serial.println(analogInput[i]);
+  }
+  Serial.println(" ");
 }
 
 void loop() {
-  PORTB ^= 0xFF;
+  
 
-  while ((PIND & 0xFF) != 0b11111011) {
-  }
-
-  PORTB = 0b10101010;
-
-  while ((PIND & 0xFF) != 0b11110111) {
-  }
-
-  for (uint8_t i = 0; i < 8; i++) {
-    PORTB = (1 << i);
-    delay(100);
-  }
-
-  PORTB ^= 0xFF;
-  delay(1000);
-
-  for (uint8_t i = 0; i < 8; i++) {
-    PORTB = (PORTB << 1) | (PORTB >> 7);
-    delay(100);
-  }
-
-  PORTB = 0x00;
 }
