36c3b5ca6b8c93131a0d7dc82f15fb77ea3240cf
mihail-gosman <mihailc.gosman@gmail.com>
2024-09-06 20:47:26 +0200

    Update firmware.ino

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,29 +1,24 @@
 
 
-void interruptSetup() {
-  TCCR2A = 0;
-  TCCR2B = 0;
-
-  TCCR2B |= (1 << CS20);
-  TIMSK2 |= (1 << TOIE2);
-
-  TCNT2 = 0;
-  
-}
-
-ISR(TIMER2_OVF_vect) {
-  Serial.println(micros());
-}
+int analogPins[] = { A0, A1, A2, A3, A4, A5 };
+int a[6];
+unsigned long delta;
 
 
 void setup() {
   Serial.begin(115200);
-
-  interruptSetup();
-
 }
 
 
 void loop() {
-  ;
+  delta = micros();
+  
+  for(int i=0; i<6; i++) {
+    a[i] = analogRead(analogPins[i]);
+
+  }
+  delta = micros() - delta;
+  if(delta > 740)
+  Serial.println(delta);
+
 }
