413f794bc36316686828295aa9db4ac24bdea59b
mihail-gosman <mihailc.gosman@gmail.com>
2024-09-08 20:47:36 +0200

    Update firmware.ino

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,23 +2,47 @@
 
 int analogPins[] = { A0, A1, A2, A3, A4, A5 };
 int a[6];
+int count = 0;
 unsigned long delta;
+int analogInputs[6];
+
+
+void timerInit() {
+  TCCR2A = 0;
+  TCCR2B = 0;
+  
+  TCCR2B |= (1 << CS21);
+  TIMSK2 |= (1 << TOIE2);
+  TCNT2 = 0;
+  Serial.println("lma");
+}
+
+
+ISR(TIMER2_OVF_vect) {
+  delta = micros() - delta;
+  
+  for(int i=0; i<7; i++){
+    analogInputs[i] = analogRead(analogPins[i]);
+    Serial.println(analogInputs[i]);
+  }
+  Serial.println("_______");
+  Serial.println(delta);
+  
+  delta = micros();
+}
 
 
 void setup() {
   Serial.begin(115200);
+  timerInit();
 }
 
 
-void loop() {
-  delta = micros();
-  
-  for(int i=0; i<6; i++) {
+void loop() {  
+  /*for(int i=0; i<1; i++) {
     a[i] = analogRead(analogPins[i]);
 
-  }
-  delta = micros() - delta;
-  if(delta > 740)
-  Serial.println(delta);
+  }*/
+
 
 }
