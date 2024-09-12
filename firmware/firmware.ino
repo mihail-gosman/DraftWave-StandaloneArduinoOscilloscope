8c359679bb7fbc88ce9eb97ff968e8aa7455a98f
mihail-gosman <mihailc.gosman@gmail.com>
2024-09-12 16:52:14 +0200

    Update firmware.ino

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,48 +1,37 @@
 
-
 int analogPins[] = { A0, A1, A2, A3, A4, A5 };
-int a[6];
-int count = 0;
+uint16_t analogReadings[5];
 unsigned long delta;
-int analogInputs[6];
 
 
-void timerInit() {
-  TCCR2A = 0;
-  TCCR2B = 0;
-  
-  TCCR2B |= (1 << CS21);
-  TIMSK2 |= (1 << TOIE2);
-  TCNT2 = 0;
-  Serial.println("lma");
-}
-
-
-ISR(TIMER2_OVF_vect) {
-  delta = micros() - delta;
-  
-  for(int i=0; i<7; i++){
-    analogInputs[i] = analogRead(analogPins[i]);
-    Serial.println(analogInputs[i]);
+void readAllAnalogPins() {
+  for(int i=0; i<6; i++) {
+    analogReadings[i] = analogRead(analogPins[i]);
   }
-  Serial.println("_______");
-  Serial.println(delta);
-  
-  delta = micros();
 }
 
+
+void sendAnalogData() {
+  for(int i=0; i<6; i++) {
+    int value = analogReadings[i];
+    Serial.write(value);
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  timerInit();
+
 }
 
 
-void loop() {  
-  /*for(int i=0; i<1; i++) {
-    a[i] = analogRead(analogPins[i]);
+void loop() {
+  delta = millis();
+  //readAllAnalogPins();
+  analogReadings[1] = 30;
+  sendAnalogData();
 
-  }*/
-
+  delta = millis() - delta;
+  
+  Serial.println(delta);
 
 }
